849dbc39f1dd6a7116ebb3e6d72857d4deaac1b2
a1dotls <meeae9408@gmail.com>
2023-03-21 22:35:21 +0900

    add. 빈 이름으로  조회되지 않을 때 테스트 추가

--- src/test/java/hello/core2/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core2/beanfind/ApplicationContextBasicFindTest.java
@@ -6,9 +6,11 @@ import hello.core2.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationContextBasicFindTest {
 
@@ -33,5 +35,12 @@ public class ApplicationContextBasicFindTest {
     void findBeanByName2() {
         MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+        assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("xxxxx", MemberService.class));
+
     }
 }
